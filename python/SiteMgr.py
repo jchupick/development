34891a4aa807b27b26c16c4014e72928e3ef6236
James (JB) Chupick <31314756+jchupick@users.noreply.github.com>
2019-05-06 12:43:32 -0400

    Update SiteMgr.py

--- python/SiteMgr.py
+++ python/SiteMgr.py
@@ -5,17 +5,27 @@ import subprocess                   # To be able to issue command line commands
 import json                         # To convert a Python dictionary object to a JSON stream
 import argparse                     # Python command line argument parser
 import xml.etree.ElementTree        # For parsing XML files
+from operator import xor
 
 ###############################################################################################
 # Setup command line argument parsing
 ###############################################################################################
 parser = argparse.ArgumentParser()
-parser.add_argument('--filename', required=True, help='File containing list of Web servers', metavar='<filename>')
+parser.add_argument('--filename', required=False, help='File containing list of Web servers', metavar='<filename>')
+parser.add_argument('--servername', required=False, help='Comma separated list of Web servers', metavar='<servername>')
+parser.add_argument('--site', required=False, help='Site to filter results on. Only return the result if the url contains this string', metavar='<sitefilter>')
+parser.add_argument('--ip', required=False, help='IP to filter results on. Only return the result if the IP contains this string', metavar='<ipfilter>')
+parser.add_argument('--all', action="store_true", required=False, default=False, help='If defined, show DNS result for the entry beginning with the server name (which usually does NOT define a website). Default is OFF (tries to show only \"real\" websites')
 parser.add_argument('--loglevel', default='WARNING', choices=['CRITICAL','ERROR','WARNING','INFO','DEBUG'], help='Logging level (CRITICAL is lowest level of logging, DEBUG is highest)')
-if (len(sys.argv) <= 1):
+args = parser.parse_args()
+
+if (not xor(bool(args.filename), bool(args.servername))):
+    print()
+    print("Either --filename OR --servername arguments are required (but not both)")
+    print()
     parser.print_help()
     exit()
-args = parser.parse_args()
+
 ###############################################################################################
 
 ###############################################################################################
@@ -44,17 +54,25 @@ def getAppnameFromWebConfig(webconfigfilenameparam):
         returnappbinary = re.sub(r'^[.][\\](.+)', r'\1', returnappbinary)   # Get rid of leading '.\' if it's there
     except Exception as ex:
         exmessage = 'Exception::{}'.format(type(ex).__name__)
-        consoleLog.debug("{} ===> {}".format(exmessage, webconfigfilenameparam))
+        consoleLog.info("{} ===> {}".format(exmessage, webconfigfilenameparam))
 
     return returnappbinary
 
-with open(args.filename) as f:
-    file_lines = f.read().splitlines()
-
 masterdictionary   = {}
-siteserverkeydelim = '::'       # Global, unique dictionary key will look like:    sevrername::sitename
+siteserverkeydelim = '::'       # Global, unique dictionary key will look like:    servername::sitename
     
-for servername in file_lines:
+# Get the server list, either from --filename or --servername
+# (Argumewnt processing earlier ensures that only 1 of the 
+# below code blocks will run)
+server_names = []
+if (bool(args.filename)):
+    serverfile = args.filename
+    with open(serverfile) as f:
+        server_names = f.read().splitlines()
+if (bool(args.servername)):
+    server_names = args.servername.split(",")
+
+for servername in server_names:
     # Poor man's check for comments. If line doesn't 
     # start with a letter, consider it commented.
     servercheckmatch = re.search(r'^[A-z]', servername)
@@ -67,45 +85,55 @@ for servername in file_lines:
     # Run the command:  appcmd list site
     #
     ###############################################################################################
-    cmdargs = ['psexec', '\\\\' + servername, 'appcmd', 'list', 'site']
+    cmdargs = ['powershell', 'Invoke-Command', '-ScriptBlock', '{ appcmd list site }', '-ComputerName', '"{}"'.format(servername)]
     consoleLog.debug(' '.join(cmdargs))
-    appcmdcmd = subprocess.run(args = cmdargs, 
-                                  universal_newlines = True, 
-                                  stdout = subprocess.PIPE, 
-                                  stderr = subprocess.PIPE
-                                  )
+
+    try:
+        appcmdcmd = subprocess.run(args = cmdargs, 
+                                    universal_newlines = True, 
+                                    stdout = subprocess.PIPE, 
+                                    stderr = subprocess.PIPE
+                                    )
+        appcmdcmd.check_returncode()
     
-    appcmd_lines = appcmdcmd.stdout.splitlines()
+        appcmd_lines = appcmdcmd.stdout.splitlines()
 
-    for line in appcmd_lines:
-        linematch = re.search(r'SITE "(.+)" [(](.+)[)]', line)
-        if linematch:
-            sitename = linematch.group(1)
-            
-            detailsmatch = re.search(r'id:(.+?),bindings:(.+?),state:(.+?)$', linematch.group(2))
-            
-            if detailsmatch:
-                siteid       = detailsmatch.group(1)
-                sitebindings = detailsmatch.group(2)
-                sitestate    = detailsmatch.group(3)
-            
-            singlesite = {}
+        for line in appcmd_lines:
+            linematch = re.search(r'SITE "(.+)" [(](.+)[)]', line)
+            if linematch:
+                sitename = linematch.group(1)
+                if ((sitename == servername) and (args.all)):
+                    continue
+                
+                detailsmatch = re.search(r'id:(.+?),bindings:(.+?),state:(.+?)$', linematch.group(2))
+                
+                if detailsmatch:
+                    siteid       = detailsmatch.group(1)
+                    sitebindings = detailsmatch.group(2)
+                    sitestate    = detailsmatch.group(3)
+                
+                singlesite = {}
 
-            bindingslist = sitebindings.split(",")
-            for binding in bindingslist:
-                bindingnvlist = binding.split("/")
-                singlesite['binding-' + bindingnvlist[0]] = bindingnvlist[1]
+                bindingslist = sitebindings.split(",")
+                for binding in bindingslist:
+                    bindingnvlist = binding.split("/")
+                    singlesite['binding-' + bindingnvlist[0]] = bindingnvlist[1]
 
-            singlesite['name']           = sitename
-            singlesite['id']             = siteid
-            singlesite['bindingsdetail'] = sitebindings
+                singlesite['name']           = sitename
+                singlesite['id']             = siteid
+                singlesite['bindingsdetail'] = sitebindings
+                
+                singlesite['state']     = sitestate
+                singlesite['server']    = servername
+                
+                siteserverkey = sitename + siteserverkeydelim + servername
+                masterdictionary[siteserverkey] = singlesite
             
-            singlesite['state']     = sitestate
-            singlesite['server']    = servername
-            
-            siteserverkey = sitename + siteserverkeydelim + servername
-            masterdictionary[siteserverkey] = singlesite
-            
+    except Exception as ex:
+        exmessage = 'Exception::{}'.format(type(ex).__name__)
+        consoleLog.critical('{} (appcmd.exe in the system PATH for \\\\{} ?)'.format(exmessage, servername))
+        consoleLog.info(appcmdcmd.stderr)
+
     ###############################################################################################
     #
     # Run the command:  appcmd list app
@@ -113,7 +141,7 @@ for servername in file_lines:
     #   This will find the apppool and add it to the dictionary for the already located site
     #
     ###############################################################################################
-    cmdargs = ['psexec', '\\\\' + servername, 'appcmd', 'list', 'app']
+    cmdargs = ['powershell', 'Invoke-Command', '-ScriptBlock', '{ appcmd list app }', '-ComputerName', '"' + servername + '"']
     consoleLog.debug(' '.join(cmdargs))
     appcmdcmd = subprocess.run(args = cmdargs, 
                               universal_newlines = True, 
@@ -129,6 +157,9 @@ for servername in file_lines:
             pass
             sitename    = linematch.group(1)
             apppoolname = linematch.group(2)
+            
+            if ((sitename == servername) and (args.all)):
+                continue
             # Find the dictionary for this sitename in the master 
             # and add the name-value pair for apppool
             try:
@@ -147,7 +178,7 @@ for servername in file_lines:
     #   This will find apppool details and add them to the dictionary for the already located site
     #
     ###############################################################################################
-    cmdargs = ['psexec', '\\\\' + servername, 'appcmd', 'list', 'apppool']
+    cmdargs = ['powershell', 'Invoke-Command', '-ScriptBlock', '{ appcmd list apppool }', '-ComputerName', '"' + servername + '"']
     consoleLog.debug(' '.join(cmdargs))
     appcmdcmd = subprocess.run(args = cmdargs, 
                               universal_newlines = True, 
@@ -193,7 +224,7 @@ for servername in file_lines:
     #   This will find the directory and add it to the dictionary for the already located site
     #
     ###############################################################################################
-    cmdargs = ['psexec', '\\\\' + servername, 'appcmd', 'list', 'vdir']
+    cmdargs = ['powershell', 'Invoke-Command', '-ScriptBlock', '{ appcmd list vdir }', '-ComputerName', '"' + servername + '"']
     consoleLog.debug(' '.join(cmdargs))
     appcmdcmd = subprocess.run(args = cmdargs, 
                               universal_newlines = True, 
@@ -209,6 +240,10 @@ for servername in file_lines:
             pass
             sitename    = linematch.group(1)
             directory   = linematch.group(2)
+            consoleLog.debug('sitename: {} directory: {}'.format(sitename, directory))
+            if ((sitename == servername) and (args.all)):
+                continue
+            
             # Find the dictionary for this sitename in the master 
             # and add the name-value pair for apppool
             try:
@@ -221,15 +256,32 @@ for servername in file_lines:
                 # Pull the application exe or dll from web.config, then 
                 # find the defined exe or dll within web.config
                 webconfigfilename  = uncdirectory + '\\web.config'
+                
+                consoleLog.debug('Calling: getAppnameFromWebConfig({})'.format(webconfigfilename))
                 tempdict['appbinary'] = getAppnameFromWebConfig(webconfigfilename)
             except KeyError as ex:
                 exmessage = 'Exception during appcmd vdir::{}'.format(type(ex).__name__)
                 consoleLog.warning("Key: {} does NOT exist. {}".format(siteserverkey, exmessage))
             
 for key in masterdictionary.keys():
+    tempdict = masterdictionary[key]
+    
+    filteronsite = False
+    filteronip   = False
+    if (bool(args.site)):
+        sitefiltermatch = re.search(args.site, tempdict['bindingsdetail'])
+        filteronsite    = True
+    if (bool(args.ip)):
+        ipfiltermatch   = re.search(args.ip, tempdict['bindingsdetail'])
+        filteronip      = True
+
+    if ((filteronsite) and (not sitefiltermatch)):
+        continue
+    if ((filteronip)   and (not ipfiltermatch)):
+        continue
+        
     print('{:18} {:52}'.format('Site Key:', key))
     print('-----------------------------------------------------------')
-    tempdict = masterdictionary[key]
     for key1 in tempdict.keys():
         print('{:18} {:52}'.format(key1, tempdict[key1]))
     print()
