0b04f69553b32f36b5707c85971c7cbfa613ab4f
James B Chupick <31314756+jchupick@users.noreply.github.com>
2020-11-23 18:13:26 -0500

    Create Get-VMDetail.ps1

---
+++ PowerShell/VMWare/Get-VMDetail.ps1
@@ -0,0 +1,49 @@
+#
+#
+#
+param(
+    [string]$Name = '*',
+    [switch]$NoStream
+)
+
+$objectCollection = @()
+
+$vms = Get-VM -Name $Name
+
+foreach ($vmitem in $vms)
+{
+    $ipCollection = @()
+    
+    foreach ($ip in $vmitem.Guest.IPAddress)
+    {
+        # Poor man's regex for IPv4 IPs, but good enough to filter out IPv6
+        if ($ip -match '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+        {
+            $ipCollection += $ip
+        }
+    }
+    
+    $newobject = [PSCustomObject]@{
+        Name        = $vmitem.Name
+        PowerState  = $vmitem.PowerState
+        Id          = $vmitem.Id
+        State       = $vmitem.Guest.State
+        #IP          = $vmitem.Guest.IPAddress
+        IP          = $ipCollection
+        OS          = $vmitem.Guest.OSFullName
+    }
+    if ($NoStream)
+    {
+        $objectCollection += $newobject
+    }
+    else
+    {
+        $newobject
+    }
+}
+
+# Send it down the pipeline
+if ($NoStream)
+{
+    $objectCollection | Sort-Object -Property Name
+}
